93f68c9dded4ebf44f871361024e5755983ae6af
zxowl <zxowl@qq.com>
2019-05-13 15:53:44 +0800

    提交一个测试类文件 tzhu

--- javawebv002/src/main/java/com/test/TestZhu.java
+++ javawebv002/src/main/java/com/test/TestZhu.java
@@ -7,6 +7,8 @@ public class TestZhu {
         System.out.println("第一次同步代码类文件 ");
         System.out.println("第一次同步代码类文件 ");
         System.out.println("第一次同步代码类文件 ");
+        System.out.println("第一次同步代码类文件 ");
+        System.out.println("第一次同步代码类文件 ");
 
 
 
