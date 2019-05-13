d594dcec0300f21ef8e882b759c1ccbc89ac6ea6
wanghuanqiang <1205549369@qq.com>
2019-05-13 16:42:28 +0800

    提交一个测试类文件 wang

---
+++ javawebv002/src/main/java/com/test/TestWang.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class TestWang {
+    public static void main(String[] args) {
+        System.out.println("第一次同步代码类文件");
+        System.out.println("第一次同步代码类文件");
+        System.out.println("第一次同步代码类文件");
+        System.out.println("第一次同步代码类文件");
+    }
+
+}
